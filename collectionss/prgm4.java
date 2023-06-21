bd86a5c2cc6886278cc6c1dd7ba7c94f9fbb13aa
rahulramv3098 <rahulramv3098@gmail.com>
2023-06-21 14:55:05 +0530

    Create prgm4.java

---
+++ collectionss/prgm4.java
@@ -0,0 +1,15 @@
+package collectionss;
+import java.util.*;
+class prgm4{  
+   public static void main(String args[]){  
+        
+      ArrayList<String> gym=new ArrayList<String>();  
+      gym.add("Dumbbell");
+      gym.add("Situpbar");
+      gym.add("Forearm Strengthner");
+      gym.add("Weights");
+      gym.add("Barbell");
+      gym.add("Powerack");
+      System.out.println(gym.contains("Weights"));
+}
+}
