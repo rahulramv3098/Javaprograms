20bb0ca84f24497fcfea0e91196dd37fa9fe526f
rahulramv3098 <rahulramv3098@gmail.com>
2023-06-21 14:52:52 +0530

    Create prgm3.java

---
+++ collectionss/prgm3.java
@@ -0,0 +1,13 @@
+package collectionss;
+import java.util.*;
+class prgm3{  
+   public static void main(String args[]){  
+        
+      ArrayList<String> gym=new ArrayList<String>();  
+      gym.add("Dumbbell");
+      gym.add("Situpbar");
+      gym.add("Forearm Strengthner");
+      gym.add("Weights");
+      System.out.println(gym);
+}
+}
